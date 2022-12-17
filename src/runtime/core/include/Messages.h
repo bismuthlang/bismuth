14f332b0d9bbd737ea0d3cf0ff2b68731dbe9f38
ahf <41027529+ahfriedman@users.noreply.github.com>
2022-12-17 16:57:48 -0500

    Added imp files

---
+++ src/runtime/core/include/Messages.h
@@ -0,0 +1,22 @@
+#pragma once
+/**
+ * @file Messages.h
+ * @author Alex Friedman (ahfriedman.com)
+ * @brief Manages the various types of messages in a queue
+ * @version 0.1
+ * @date 2022-12-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <variant>
+
+struct Value {
+    void * v; 
+};
+
+struct START_LOOP {}; 
+struct END_LOOP {}; 
+struct SEL  {}; //FIXME: WILL NEED WORK 
+
+typedef std::variant<Value, START_LOOP, END_LOOP, END_LOOP, SEL> Message; 
---
+++ src/runtime/core/include/StateManager.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "IPCBuffer.h"
+#include "Messages.h"
+
+#include <map>
+#include <thread>
+#include <iostream>
+#include <iterator>
+
+std::map<unsigned int, IPCBuffer<Message> *> State;
+
+std::map<unsigned int, unsigned int> LookupOther; // FIXME: NAME BETTER
+
+void Execute(void (*func)(unsigned int))
+{
+    IPCBuffer<Message> *aIn = new IPCBuffer<Message>();
+    IPCBuffer<Message> *aOut = new IPCBuffer<Message>();
+
+    unsigned int idIn = State.size();
+
+    State.insert({idIn, aIn});
+
+    unsigned int idOut = State.size();
+
+    State.insert({idOut, aOut});
+
+    LookupOther.insert({idOut, idIn});
+    LookupOther.insert({idIn, idOut});
+
+    std::thread t(func, idIn);
+    // func(idIn); //FIXME: RUN IN THREAD!
+}
+
+void WriteChannel(unsigned int aId, void *value)
+{
+    auto i_oAId = LookupOther.find(aId);
+
+    if (i_oAId == LookupOther.end())
+        return; // FIXME: DO BETTER
+
+    unsigned int oAId = i_oAId->second;
+
+
+
+    auto i_buffer = State.find(oAId);
+
+    if (i_buffer == State.end())
+        return; // FIXME: DO BETTER
+
+    Value v; 
+    v.v = value; 
+
+    i_buffer->second->enqueue(v);
+}
+
+
+void* ReadChannel(unsigned int aId)
+{
+    auto i_buffer = State.find(aId);
+
+    if (i_buffer == State.end())
+        return nullptr; // FIXME: DO BETTER
+
+    return std::get<Value>(i_buffer->second->dequeue()).v; //FIXME: RENAME TO BE MESSAGE VALUE OR SOMETHING
+}
