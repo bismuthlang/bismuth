8c65501f9c6e1a20a77432d1c538ad9f96f21d74
ahf <41027529+ahfriedman@users.noreply.github.com>
2023-03-25 10:50:24 -0400

    Commented out

--- src/codegen/include/CodegenVisitor.h
+++ src/codegen/include/CodegenVisitor.h
@@ -208,7 +208,7 @@ public:
         {
             // e->accept(this);
             this->accept(e);
-            module->dump();
+            // module->dump();
         }
 
         // If we are a PROC, make sure to add a return type (if we don't already have one)
