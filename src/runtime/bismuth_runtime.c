364d0fcd867d2a0748f52322f706bb9ed204f3a9
ahf <41027529+ahfriedman@users.noreply.github.com>
2023-06-04 13:36:20 -0400

    cleanup

--- src/runtime/bismuth_runtime.c
+++ src/runtime/bismuth_runtime.c
@@ -1,12 +1,12 @@
 /**
  * @file bismuth_runtime.c
  * @author Alex Friedman (ahfriedman.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-02-04
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,50 +15,54 @@
 void program(unsigned int i);
 
 unsigned int Execute(void (*func)(unsigned int));
-void* ReadChannel(unsigned int aId);
+void *ReadChannel(unsigned int aId);
 
-void waitForAllToFinish(); 
+void waitForAllToFinish();
 
 int argCount;
 char **args;
 
 /**
  * @brief main program that calls the program() function
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   argCount = argc;
   args = argv;
 
   unsigned int idOut = Execute(&program);
-  
-  void * ptr = ReadChannel(idOut);
+
+  void *ptr = ReadChannel(idOut);
 
   waitForAllToFinish();
-  return 0; 
+  return 0;
 }
 
 /**
  * @brief Returns the number of arguments on the command line.
  *  There is always one, the program name
- * 
+ *
  * @return int the number of arguments
  */
-int getArgCount() {
+int getArgCount()
+{
   return argCount;
 }
 
 /**
  * @brief Get an argument from the command line as a string
- * 
- * @param i 
- * @return char* 
+ *
+ * @param i
+ * @return char*
  */
-char *getStrArg(int i) {
-  if (i < 0 || i >= argCount) {
+char *getStrArg(int i)
+{
+  if (i < 0 || i >= argCount)
+  {
     fprintf(stderr, "Attempt to access an argument out of bounds -- aborting!\n");
     exit(-1);
   }
@@ -67,12 +71,14 @@ char *getStrArg(int i) {
 
 /**
  * @brief Get an argument from the command line as an integer
- * 
- * @param i 
- * @return int 
+ *
+ * @param i
+ * @return int
  */
-int getIntArg(int i) {
-  if (i < 0 || i >= argCount) {
+int getIntArg(int i)
+{
+  if (i < 0 || i >= argCount)
+  {
     fprintf(stderr, "Attempt to access an argument out of bounds -- aborting!\n");
     exit(-1);
   }
