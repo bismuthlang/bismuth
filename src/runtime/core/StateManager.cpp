3f3c04ac34f040002459e393aff8944171f8b009
ahf <41027529+ahfriedman@users.noreply.github.com>
2023-08-27 10:53:43 -0400

    Cleanup

--- src/runtime/core/StateManager.cpp
+++ src/runtime/core/StateManager.cpp
@@ -1,6 +1,5 @@
 #include "StateManager.h"
 
-
 extern "C" void waitForAllToFinish()
 {
     std::unique_lock<std::mutex> lock{running_mutex};
@@ -54,14 +53,19 @@ extern "C" unsigned int Execute(void (*func)(unsigned int))
 
 std::string Message2String(Message m)
 {
-    return std::visit(overload{
-        [](START_LOOP& )    { return std::string("START-LOOP "); },
-        [](END_LOOP& )      { return std::string("END-LOOP "); },
-        [](Value& )         { return std::string("Val "); },
-        [](SEL& s)          { return "SEL[" + std::to_string(s.i) + "] "; },
-        [](SKIP& s)         { return "SKIP[" + std::to_string(s.i) + "] "; },
-        [](CLOSE& s)        { return "CLOSE " + std::to_string(s.i) + "] "; }
-    }, m);
+    return std::visit(overload{[](START_LOOP &)
+                               { return std::string("START-LOOP "); },
+                               [](END_LOOP &)
+                               { return std::string("END-LOOP "); },
+                               [](Value &)
+                               { return std::string("Val "); },
+                               [](SEL &s)
+                               { return "SEL[" + std::to_string(s.i) + "] "; },
+                               [](SKIP &s)
+                               { return "SKIP[" + std::to_string(s.i) + "] "; },
+                               [](CLOSE &s)
+                               { return "CLOSE " + std::to_string(s.i) + "] "; }},
+                      m);
 }
 
 void debug()
@@ -77,7 +81,7 @@ void debug()
         while (!copy_queue.empty())
         {
             Message m = copy_queue.front();
-            std::cout << Message2String(m); 
+            std::cout << Message2String(m);
             copy_queue.pop_front();
         }
         std::cout << std::endl;
@@ -107,11 +111,11 @@ IPCBuffer<Message> *getWriteQueue(unsigned int aId)
         throw "Preservation Error: WriteHelper could not locate buffer to write to!";
     }
 
-    return i_buffer->second; 
+    return i_buffer->second;
 }
 
 void WriteHelper(unsigned int aId, Message m)
-{ 
+{
     IPCBuffer<Message> *writeQueue = getWriteQueue(aId);
 
     // if (DEBUG)
@@ -160,7 +164,7 @@ extern "C" void WriteChannel(unsigned int aId, uint8_t *value)
 
 extern "C" void WriteProjection(unsigned int aId, unsigned int selVal)
 {
-    SEL s(selVal); 
+    SEL s(selVal);
     WriteHelper(aId, s);
 }
 
@@ -208,19 +212,22 @@ extern "C" bool _OC_isPresent(unsigned int aId)
     IPCBuffer<Message> *readQueue = getReadQueue(aId);
     std::optional<Message> mOpt = readQueue->peakNow();
 
-    if(!mOpt) return false; 
-    
-    Message m = mOpt.value(); 
+    if (!mOpt)
+        return false;
 
-    if (std::holds_alternative<START_LOOP>(m)) return true; 
-    else if(std::holds_alternative<END_LOOP>(m)) return false; 
+    Message m = mOpt.value();
+
+    if (std::holds_alternative<START_LOOP>(m))
+        return true;
+    else if (std::holds_alternative<END_LOOP>(m))
+        return false;
 
     throw "Preservation Error: _OC_isPresent got something besides START_LOOP or END_LOOP!";
 }
 
 extern "C" bool ShouldAcceptWhileLoop(unsigned int aId)
 {
-    IPCBuffer<Message> *readQueue = getReadQueue(aId); 
+    IPCBuffer<Message> *readQueue = getReadQueue(aId);
     Message m = readQueue->peak();
 
     if (std::holds_alternative<START_LOOP>(m))
@@ -276,125 +283,102 @@ extern "C" void WeakenChannel(unsigned int aId)
     WriteHelper(aId, v);
 }
 
-
-
-
-
-
-extern "C" unsigned int _ArrayToChannel(uint8_t * array[], unsigned int len)
+extern "C" unsigned int _ArrayToChannel(uint8_t *array[], unsigned int len)
 {
     exec_mutex.lock();
-    IPCBuffer<Message> *aRead = new IPCBuffer<Message>(); 
+    IPCBuffer<Message> *aRead = new IPCBuffer<Message>();
     unsigned int id = State.size();
 
     State.insert({id, aRead});
 
-
-    for(unsigned int i = 0; i < len; i++)
+    for (unsigned int i = 0; i < len; i++)
     {
         START_LOOP s;
         Value v(array[i]);
 
         aRead->enqueue(s);
-        aRead->enqueue(v); 
+        aRead->enqueue(v);
     }
-    
-    END_LOOP end; 
-    aRead->enqueue(end); 
+
+    END_LOOP end;
+    aRead->enqueue(end);
 
     exec_mutex.unlock();
 
-    return id; 
+    return id;
 }
 
-
-
-
-
-
-
-void _ClearChannel(IPCBuffer<Message> * readQueue) //unsigned int aId)
+void _ClearChannel(IPCBuffer<Message> *readQueue) // unsigned int aId)
 {
-    // IPCBuffer<Message> *readQueue = getReadQueue(aId);
-    std::optional<Message> mOpt = readQueue->peakNow();
-
-    if(mOpt)
-    {
-        Message frontMsg = mOpt.value(); 
-
-        if(std::holds_alternative<SKIP>(frontMsg))
+    readQueue->operateOn([](std::deque<Message> &q)
         {
-            SKIP skipMsg = std::get<SKIP>(frontMsg);
+            if (q.empty())
+                return;
 
-            readQueue->operateOn([skipTo = skipMsg.i](std::deque<Message>& q){
-                unsigned int skipAmt = skipTo; 
+            if (!std::holds_alternative<SKIP>(q.front()))
+                return;
 
-                while(!q.empty())
+            unsigned int skipAmt = 0; // skipTo;
+
+            while (!q.empty())
+            {
+                Message frontMsg = q.front();
+
+                if (std::holds_alternative<SKIP>(frontMsg))
                 {
-                    Message frontMsg = q.front(); 
+                    // If first-ish message is skip, then
+                    // update amount skipped to the max, and continue
+                    q.pop_front();
+                    SKIP skipMsg = std::get<SKIP>(frontMsg);
+                    skipAmt = std::max(skipAmt, skipMsg.i);
+                }
+                else if (std::holds_alternative<CLOSE>(frontMsg))
+                {
+                    CLOSE closeMsg = std::get<CLOSE>(frontMsg);
 
-                    if(std::holds_alternative<SKIP>(frontMsg))
+                    if (skipAmt == closeMsg.i)
                     {
-                        // If first-ish message is skip, then 
-                        // update amount skipped to the max, and continue
-                        q.pop_front(); 
-                        SKIP skipMsg = std::get<SKIP>(frontMsg);
-                        skipAmt = std::max(skipAmt, skipMsg.i);
+                        // If close # = skip #, then we are done (both programs hit same sync point)
+                        // and we can clear out the close.
+                        q.pop_front();
+                        return;
                     }
-                    else if(std::holds_alternative<CLOSE>(frontMsg))
-                    {
-                        CLOSE closeMsg = std::get<CLOSE>(frontMsg); 
 
-                        if(skipAmt == closeMsg.i)
-                        {
-                            // If close # = skip #, then we are done (both programs hit same sync point)
-                            // and we can clear out the close. 
-                            q.pop_front();
-                            return; 
-                        }
-                        
-                        if(skipAmt > closeMsg.i)
-                        {
-                            // If we skip past the close, then ignore the close
-                            q.pop_front(); 
-                        }
-                        else  // skipAmt < closeMessage.i
-                        {
-                            // If the close is for more than we skip, then preserve the close
-                            // and return before we re-add the skip message to the front 
-                            // of the queue 
-                            return; 
-                        }
-                    }
-                    else 
+                    if (skipAmt > closeMsg.i)
                     {
-                        q.pop_front(); // FIXME: MAY HAVE TO FREE IF VALUE & DEAL WITH HIGHER-ORDER STUFF!
+                        // If we skip past the close, then ignore the close
+                        q.pop_front();
+                    }
+                    else // skipAmt < closeMessage.i
+                    {
+                        // If the close is for more than we skip, then preserve the close
+                        // and return before we re-add the skip message to the front
+                        // of the queue
+                        return;
                     }
                 }
+                else
+                {
+                    q.pop_front(); // FIXME: MAY HAVE TO FREE IF VALUE & DEAL WITH HIGHER-ORDER STUFF!
+                }
+            }
 
-                SKIP s(skipAmt); 
-                q.push_front(s);
-
-            });
-        }
-    }
+            SKIP s(skipAmt);
+            q.push_front(s);
+        });
 }
 
 extern "C" void _PreemptChannel(unsigned int aId, unsigned int skipTo)
 {
     IPCBuffer<Message> *readQueue = getReadQueue(aId);
 
-    readQueue->operateOn([skipTo](std::deque<Message>& q){
-        q.push_front(SKIP(skipTo));
-    });
+    readQueue->operateOn([skipTo](std::deque<Message> &q)
+                         { q.push_front(SKIP(skipTo)); });
 
+    IPCBuffer<Message> *writeQueue = getWriteQueue(aId);
+    writeQueue->operateOn([skipTo](std::deque<Message> &q)
+                          { q.push_front(CLOSE(skipTo)); });
 
-    IPCBuffer<Message>* writeQueue = getWriteQueue(aId); 
-    writeQueue->operateOn([skipTo](std::deque<Message>& q){
-        q.push_front(CLOSE(skipTo));
-    });
-
-    _ClearChannel(readQueue); 
+    _ClearChannel(readQueue);
     _ClearChannel(writeQueue);
-
 }
