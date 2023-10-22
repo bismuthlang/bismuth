634f59d8f5075b01e3f8e42977629ebe6baaf6cc
ahf <41027529+ahfriedman@users.noreply.github.com>
2023-10-22 10:35:34 -0400

    Added symbol utils

---
+++ src/symbol/SymbolUtils.cpp
@@ -0,0 +1,1 @@
+#include "SymbolUtils.h"
---
+++ src/symbol/include/SymbolUtils.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/**
+ * @file SymbolUtils.h
+ * @author Alex Friedman (ahf.dev)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+enum DisplayMode {
+    C_STYLE, 
+    MATH_STYLE
+};
