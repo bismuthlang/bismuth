f2ea470aff9bc2d68c09a8b654d28caf96a048c5
Nico T <40475864+Nikoolaid@users.noreply.github.com>
2024-02-05 16:52:32 -0500

    Merge

--- src/ltl/include/LtlMonitor.h
+++ src/ltl/include/LtlMonitor.h
@@ -1,7 +1,31 @@
 #pragma once
 
-class LtlMonitor
+#include <optional>
+#include <variant>
+#include "BismuthParser.h"
+#include "LTLMonitorDef.h"
+#include "DFA.h"
+#include "Symbol.h"
+#include "TypedAST.h"
+#include "BismuthErrorHandler.h"
+#include "GenCx.h"
+
+class LTLMonitor
 {
+  private:
+    LTLMonitorDef def;
+    Symbol *progSym;
+    Symbol *monSym;
+    antlr4::Token *rootTok = NULL;
+
  public:
-    static std::optional<LtlMonitor> getMonitor(ProgramDefinitionContext *ctx);
-}
+    LTLMonitor(LTLMonitorDef def, Symbol *progSym, Symbol *monSym);
+
+    std::variant<TProgramDefNode *, ErrorChain *> gen(BismuthErrorHandler &errorHandler, Symbol *abortSym);
+
+    void resolveMonitorType();
+
+  private:
+    const TypeProgram *getProgTy();
+};
+
