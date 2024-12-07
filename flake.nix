f8ddac556b78ec80f6dff67379b36d2e0ff84878
ahf <41027529+ahfriedman@users.noreply.github.com>
2024-12-06 19:59:22 -0600

    attempting to get flake working

--- flake.nix
+++ flake.nix
@@ -8,33 +8,28 @@
     let 
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      #stdenv = pkgs.llvmPackages_12.stdenv;
+      llvmEnv = pkgs.overrideCC pkgs.stdenv (
+        pkgs.llvmPackages_14.libcxxStdenv.cc.override { 
+          inherit (pkgs.llvmPackages_14) bintools;
+      });
     in {
-      devShell.${system} = pkgs.llvmPackages_14.libcxxStdenv.mkDerivation {
+      devShell.${system} = llvmEnv.mkDerivation {
       name = "bismuth";
-      #(pkgs.mkShell.override {
-#        stdenv = pkgs.llvmPackages_12.stdenv;
- #     }) {
         nativeBuildInputs = [
-          pkgs.stdenv.override {
-            LD = pkgs.lld12;
-          }
-          pkgs.llvmPackages_12.libllvm
-          pkgs.lld_12
-         # pkgs.llvmPackages_12.libclang
-         # pkgs.llvmPackages_12.libstdcxxClang
-#          pkgs.llvmPackages_12.libcxxClang
+          pkgs.clang-tools_12 
           pkgs.cmake
           pkgs.pkg-config
           pkgs.libossp_uuid
           pkgs.unzip
+          pkgs.libxml2
         ] ++ [
-            pkgs.llvmPackages_12.libllvm
+pkgs.llvmPackages_12.libllvm
         ];
+         LDFLAGS = [ "-L${pkgs.llvmPackages_12.libllvm.dev}/lib" ];
        #  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-#        shellHook = ''
-#    export LD_LIBRARY_PATH="${pkgs.llvmPackages_12.libstdcxxClang}/lib:${pkgs.libossp_uuid}/lib"
-#  '';
+        shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.llvmPackages_12.libstdcxxClang}/lib:${pkgs.libossp_uuid}/lib"
+  '';
        # shellHook = ''
     #export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib/:${pkgs.llvmPackages_12.libclang}/lib:${pkgs.llvmPackages_12.libstdcxxClang}/lib:${pkgs.libossp_uuid}/lib"
 #  '';
