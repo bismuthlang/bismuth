15c239e679607f995fcf7bde9cdfbe43176dfe92
ahf <41027529+ahfriedman@users.noreply.github.com>
2024-12-08 11:21:04 -0600

    WIP: Nix Flake  - Flake is currently able to build the project, but is unable to link. All associated libraries appear to be getting correctly passed to the linker.

--- flake.nix
+++ flake.nix
@@ -9,30 +9,27 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       llvmEnv = pkgs.overrideCC pkgs.stdenv (
-        pkgs.llvmPackages_14.libcxxStdenv.cc.override { 
-          inherit (pkgs.llvmPackages_14) bintools;
+        pkgs.llvmPackages_12.libcxxStdenv.cc.override { 
+           inherit (pkgs.llvmPackages_12) bintools;
       });
     in {
       devShell.${system} = llvmEnv.mkDerivation {
       name = "bismuth";
         nativeBuildInputs = [
-          pkgs.clang-tools_12 
+          pkgs.ninja
+          pkgs.clang-tools_12
           pkgs.cmake
           pkgs.pkg-config
-          pkgs.libossp_uuid
+          pkgs.jdk17
           pkgs.unzip
-          pkgs.libxml2
-        ] ++ [
-pkgs.llvmPackages_12.libllvm
         ];
-         LDFLAGS = [ "-L${pkgs.llvmPackages_12.libllvm.dev}/lib" ];
-       #  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-        shellHook = ''
-    export LD_LIBRARY_PATH="${pkgs.llvmPackages_12.libstdcxxClang}/lib:${pkgs.libossp_uuid}/lib"
-  '';
-       # shellHook = ''
-    #export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib/:${pkgs.llvmPackages_12.libclang}/lib:${pkgs.llvmPackages_12.libstdcxxClang}/lib:${pkgs.libossp_uuid}/lib"
-#  '';
+        buildInputs = [
+          pkgs.llvmPackages_12.libllvm
+          pkgs.libossp_uuid
+          pkgs.libxml2
+          pkgs.zlib
+        ];
+       # LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_12.libllvm ]}";
       };
     };
   
