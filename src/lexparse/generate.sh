bbed7aa34539c961106977096b137d7a6274e108
ahf <41027529+ahfriedman@users.noreply.github.com>
2025-02-05 19:19:43 -0600

    nix: update build

--- src/lexparse/generate.sh
+++ src/lexparse/generate.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
-
-if [ ! -f ../generated/$1Parser.cpp ] || [ "$1.g4" -nt "../generated/$1Parser.cpp" ]; 
-then 
+rm ./parser_sources.h
+if [ ! -f ../generated/$1Parser.cpp ] || [ "$1.g4" -nt "../generated/$1Parser.cpp" ];
+then
   echo ">>> Generating <<<"
   pwd
-  java -jar ../../antlr/antlr4.jar -Dlanguage=Cpp -visitor -listener -o ../generated $1.g4 
+  java -jar ../../antlr/antlr4.jar -Dlanguage=Cpp -visitor -listener -o ../generated $1.g4
   touch parser_sources.h
   echo ">>> Parser Generation Complete <<<"
 fi
+touch parser_sources.h
